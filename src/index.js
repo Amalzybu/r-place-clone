89c5cd7cf0d399f4cfb129fbe0760cc643d16a12
amalk_beacon <amalk@beaconinfo.tech>
2022-09-28 08:02:31 +0530

    yeah another bullet

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ head.oninput = () => { copy.put(head.value) };
 copy.on((data) => { even = data });
 
 var scene = new THREE.Scene();
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 var camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
 camera.position.set(0, 0, 10);
 camera.lookAt(scene.position);
@@ -43,27 +45,41 @@ function drawRectangle(index, width, height, color) {
   ctx.fillRect(x_, y_, w_, h_);
   map.needsUpdate = true;
 }
-for (let index = 0; index < 121; index++) {
 
-  if(index%2===0){
-    drawRectangle(index, 1, 1, even);
-  }
-  else{
-    drawRectangle(index, 1, 1, "gray");
-
-  }
-}
 
 var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(11, 11), new THREE.MeshBasicMaterial({
   color: "white",
   map: map
 }));
+raycaster.setFromCamera(mouse, camera);
+
 scene.add(plane);
 
+document.addEventListener(
+  "click",
+  event => {
+  mouse.x = event.clientX / window.innerWidth * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
+  // camera.position.x = mouse.x;
+  // camera.position.z = mouse.y;
+  // console.log("ggggggggggggg",mouse)
+
+  },
+  false
+);
 
 renderer.setAnimationLoop(() => {
+  for (let index = 0; index < 121; index++) {
 
+    if(index%2===0){
+      drawRectangle(index, 1, 1, even);
+    }
+    else{
+      drawRectangle(index, 1, 1, "gray");
+  
+    }
+  }
 
   renderer.render(scene, camera);
 });
